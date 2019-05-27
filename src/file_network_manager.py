ab224f77c5e2da85b0c8cb72adf0cac9734b95e5
Alex <a.henkel@mgga.de>
2019-05-27 17:07:03 +0200

    Upload ssh key function

--- src/file_network_manager.py
+++ src/file_network_manager.py
@@ -1,7 +1,7 @@
 from paramiko import SSHClient, AutoAddPolicy
 from paramiko.ssh_exception import AuthenticationException, SSHException
 from scp import SCPClient
-from tkinter import Frame, Label, ttk, StringVar, Tk, Toplevel
+from tkinter import Frame, Label, ttk, StringVar, Tk, Toplevel, RIGHT, LEFT
 
 
 class FileNetworkManager():
@@ -16,12 +16,21 @@ class FileNetworkManager():
         self.password_field = StringVar()
         self.main_frame = None
 
+
     def close_password_input_window(self):
         self.main_frame.destroy()
         self.root.destroy()
         print("Close window")
         self.password = self.password_field.get()
         self.connect_to_server()
+
+    def close_and_upload_key(self):
+        self.close_password_input_window()
+        public_keyfile_name = self.key_filename + ".pub"
+        with open(public_keyfile_name, 'r') as key:
+            key = key.readline()
+            # print('echo "' + key + '" >> .ssh/authorized_keys')
+            self.ssh.exec_command('echo "' + key + '" >> .ssh/authorized_keys')
 
     def create(self):
         self.root = Toplevel(self.tk_root)
@@ -33,7 +42,9 @@ class FileNetworkManager():
                   textvariable=self.password_field,
                   width=100, show='*').pack(anchor="w")
 
-        ttk.Button(self.main_frame, text='Connect', command=self.close_password_input_window).pack(anchor="w", pady=(15, 0))
+        ttk.Button(self.main_frame, text='Connect', command=self.close_password_input_window).pack(side=LEFT, pady=(15, 0))
+        ttk.Button(self.main_frame, text='Connect and upload Key', command=self.close_and_upload_key).pack(side=RIGHT,
+                                                                                                   pady=(15, 0))
 
         self.main_frame.wait_window(self.main_frame)
 
@@ -45,11 +56,15 @@ class FileNetworkManager():
             self.ssh.connect(self.server, username=self.username, key_filename=self.key_filename, password=self.password)
         except AuthenticationException as e:
             print("Could not connect to server")
-            print(e)
+            # print(e)
             self.create()
         except SSHException as e:
             print("Could not connect to server")
-            print(e)
+            # print(e)
+            self.create()
+        except FileNotFoundError as e:
+            print('No keyfile')
+            # print(e)
             self.create()
 
     def upload_vacation_file(self, filename):
