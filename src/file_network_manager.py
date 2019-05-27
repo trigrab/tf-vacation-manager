538e5d26ff293dd8247a488e73f1e87dbedbc7ff
Alex <a.henkel@mgga.de>
2019-05-27 15:57:24 +0200

    added passwordlogin to manager

--- src/file_network_manager.py
+++ src/file_network_manager.py
@@ -1,38 +1,77 @@
 from paramiko import SSHClient, AutoAddPolicy
-from paramiko.ssh_exception import AuthenticationException
+from paramiko.ssh_exception import AuthenticationException, SSHException
 from scp import SCPClient
+from tkinter import Frame, Label, ttk, StringVar, Tk
 
 
-def connect_to_server(server, filename, user, key_filename=None, password=None):
-    ssh = SSHClient()
-    ssh.set_missing_host_key_policy(AutoAddPolicy())
-    #ssh.load_system_host_keys()
-    try:
-        ssh.connect(server, username=user, key_filename=key_filename, password=password)
-    except AuthenticationException as e:
-        print("Could not connect to server")
-        print(e)
-        return None
-    return ssh
+class FileNetworkManager():
 
-def upload_vacation_file(server, filename, user, key_filename=None, password=None):
-    ssh = connect_to_server(server, filename, user, key_filename, password)
+    def __init__(self, server, username, key_filename=None, password=None):
+        self.root = None
+        self.server = server
+        self.username = username
+        self.key_filename = key_filename
+        self.password = password
 
-    if ssh is None:
-        return False
 
-    scp = SCPClient(ssh.get_transport())
-    scp.put(filename)
-    scp.close()
-    return True
+    def close_password_input_window(self):
+        self.root.destroy()
 
-def delete_vacation_file(server, filename, user, key_filename=None, password=None):
-    ssh = connect_to_server(server, filename, user, key_filename, password)
 
-    if ssh is None:
-        return False
-    # ssh.set_missing_host_key_policy(AutoAddPolicy())
-    ssh.load_system_host_keys()
-    ssh.connect(server, username=user, key_filename=key_filename)
-    ssh.exec_command('rm ' + filename)
-    return True
+    def create(self):
+        self.root = Tk()
+        self.root.geometry("300x450")
+        self.root.title = "VacationManager"
+
+        main = Frame(pady=15, padx=15)
+        main.pack(expand=True, fill="both")
+
+        Label(main, justify="left", text="Password").pack(anchor="w", pady=(15, 0))
+        password_field = StringVar()
+        ttk.Entry(main,
+                  textvariable=password_field,
+                  width=100).pack(anchor="w")
+
+        ttk.Button(main, text='Connect', command=self.close_password_input_window()).pack(anchor="w", pady=(15, 0))
+
+        main.wait_window(main)
+
+        self.password = password_field.get()
+        self.connect_to_server()
+
+
+    def connect_to_server(self):
+        self.ssh = SSHClient()
+        self.ssh.set_missing_host_key_policy(AutoAddPolicy())
+        #ssh.load_system_host_keys()
+        try:
+            self.ssh.connect(self.server, username=self.username, key_filename=self.key_filename, password=self.password)
+        except AuthenticationException as e:
+            print("Could not connect to server")
+            print(e)
+            self.create()
+        except SSHException as e:
+            print("Could not connect to server")
+            print(e)
+            self.create()
+
+
+    def upload_vacation_file(self, filename):
+        self.connect_to_server()
+
+        if self.ssh is None:
+            return False
+
+        scp = SCPClient(self.ssh.get_transport())
+        scp.put(filename)
+        scp.close()
+        return True
+
+    def delete_vacation_file(self, filename):
+        self.connect_to_server()
+
+        if self.ssh is None:
+            return False
+        # ssh.set_missing_host_key_policy(AutoAddPolicy())
+        self.ssh.exec_command('rm ' + filename)
+        return True
