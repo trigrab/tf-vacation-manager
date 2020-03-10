25bc60040013e83513cceab7b18f8f3a6d901b24
LeanderBuerkin <yogh.kurt@gmail.com>
2020-03-10 20:53:49 +0100

    stupid me

--- tf_vacation_manager/src/ssh_key_generator.py
+++ tf_vacation_manager/src/ssh_key_generator.py
@@ -4,9 +4,10 @@ from cryptography.hazmat.backends import default_backend as crypto_default_backe
 
 
 
+
 def generate_key(key_path):
     # generate private/public key pair
-    key = rsa.generate_private_key(backend=default_backend(), public_exponent=65537, \
+    key = rsa.generate_private_key(backend=crypto_default_backend(), public_exponent=65537, \
         key_size=2048)
 
     # get public key in OpenSSH format
