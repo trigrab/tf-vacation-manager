c10249669fcd47141692a382b900dfecc841e9be
LeanderBuerkin <yogh.kurt@gmail.com>
2020-03-10 20:45:03 +0100

    .

--- tf_vacation_manager/src/file_network_manager.py
+++ tf_vacation_manager/src/file_network_manager.py
@@ -74,7 +74,7 @@ class FileNetworkManager:
         self.ssh = SSHClient()
         self.ssh.set_missing_host_key_policy(AutoAddPolicy())
         print("Load key file:", self.key_filename)
-        if not os.path.isfile(self.key_filename):
+        if self.key_filename is not None and not os.path.isfile(self.key_filename):
             generate_key(self.key_filename)
         # ssh.load_system_host_keys()
 
